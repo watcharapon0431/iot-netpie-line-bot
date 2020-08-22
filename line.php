b882857805dad8c60063d90db217c9bafce1380e
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-08-22 15:31:30 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'lbnR+gYQWUfgAIDhuDKw1HfLNdp9FcnrQhR9J7mjw2gh3mDknuIvtBl8kxWcy63MFZDFQcxMVwnv1chZtEPxbF1QA017XSuX9NRMOtJlt5yQXStX2RBH2Hp1+qXl14S2mWd'; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
