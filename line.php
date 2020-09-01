45e1b377c965c7c28d5b4e697e63d96b7a5d86ae
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-09-01 16:57:50 +0700

    Update line.php

--- line.php
+++ line.php
@@ -60,15 +60,8 @@ if ( sizeof($request_array['events']) > 0 )
 	   }
 	   
 	   if($text =="@บอท ขอรายชื่อนิสิตที่ส่งงาน LineBoT"){
-	    	  $url = 'https://covid19.th-stat.com/api/open/today';
-		   $ch = curl_init($url);
-		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		   $result = curl_exec($ch);
-		   curl_close($ch); 
+	    	  $url = 'https://linebot.kantit.com/list.php';
+		   $result = file_get_contents($url);		   
 		   $obj = json_decode($result);
 		   $reply_message = 'มีส่งงาน '. $obj->{'Confirmed'} .' คน ได้แก่...';
 		   //$reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'} .' คน รักษาหายแล้ว '.$obj->{'Recovered'} . ' คน';
